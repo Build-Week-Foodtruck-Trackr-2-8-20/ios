4afe19b3b02bf7066bdefb4c8f0be0f7bcf98949
Shawn Gee <shawngee@gmail.com>
2020-08-25 01:48:14 -0400

    fix core data files after merge

--- FoodTruckTracker/FoodTruckTracker/CoreData/CoreDataStack.swift
+++ FoodTruckTracker/FoodTruckTracker/CoreData/CoreDataStack.swift
@@ -2,44 +2,41 @@
 //  CoreDataStack.swift
 //  FoodTruckTracker
 //
-//  Created by Josh Kocsis on 8/20/20.
+//  Created by Shawn Gee on 8/25/20.
 //  Copyright © 2020 Josh Kocsis. All rights reserved.
 //
 
-import Foundation
 import CoreData
+import Foundation
 
 class CoreDataStack {
     static let shared = CoreDataStack()
-
+    
     lazy var container: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "FTTData")
-        container.loadPersistentStores { (_, error) in
+        container.loadPersistentStores { _, error in
             if let error = error {
                 fatalError("Failed to load persistent stores: \(error)")
             }
         }
+        
         container.viewContext.automaticallyMergesChangesFromParent = true
         return container
     }()
-
+    
     var mainContext: NSManagedObjectContext {
         container.viewContext
     }
-
+    
     func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
         var error: Error?
-
         context.performAndWait {
             do {
                 try context.save()
-            } catch let saveError as NSError {
+            } catch let saveError {
                 error = saveError
             }
         }
-
-        if let error = error {
-            throw error
-        }
+        if let error = error { throw error }
     }
 }
