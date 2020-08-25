2076afbab16c57b412ba40c314505480c5232850
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-08-24 20:01:17 -0500

    Added conveniece for core data and took out the role in core data.

---
+++ FoodTruckTracker/CoreData/Truck+Conveniece.swift
@@ -0,0 +1,31 @@
+//
+//  Truck+Conveniece.swift
+//  FoodTruckTracker
+//
+//  Created by Josh Kocsis on 8/24/20.
+//  Copyright © 2020 Josh Kocsis. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Truck {
+
+    @discardableResult convenience init(cuisineType: String,
+                                        customerRating: Int16,
+                                        customerRatingAvg: Int16,
+                                        departureTime: Date = Date(),
+                                        imageOfTruck: String,
+                                        truckLatitude: Double,
+                                        truckLongitude: Double,
+                                        context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        self.init(context: context)
+        self.cuisineType = cuisineType
+        self.customerRating = customerRating
+        self.customerRatingAvg = customerRatingAvg
+        self.departureTime = departureTime
+        self.imageOfTruck = imageOfTruck
+        self.truckLatitude = truckLatitude
+        self.truckLongitude = truckLongitude
+    }
+}
