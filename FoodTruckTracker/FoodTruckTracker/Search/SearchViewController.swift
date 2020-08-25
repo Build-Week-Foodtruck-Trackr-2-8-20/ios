7656668d51dfb55df3ad68bdd2bea66690106fd8
Shawn Gee <shawngee@gmail.com>
2020-08-25 01:30:59 -0400

    add storyboards and references

---
+++ FoodTruckTracker/FoodTruckTracker/Search/SearchViewController.swift
@@ -0,0 +1,57 @@
+//
+//  SearchViewController.swift
+//  FoodTruckTracker
+//
+//  Created by Shawn Gee on 8/24/20.
+//  Copyright © 2020 Josh Kocsis. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    @IBOutlet var resultsContainer: UIView!
+    
+    let mapVC = UIStoryboard(name: "SearchResultsMap", bundle: .main).instantiateInitialViewController()!
+    let listVC = UIStoryboard(name: "SearchResultsList", bundle: .main).instantiateInitialViewController()!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addChild(listVC)
+        resultsContainer.addSubview(listVC.view)
+        listVC.didMove(toParent: self)
+        listVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addChild(mapVC)
+        mapVC.didMove(toParent: self)
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func swapViews(_ sender: UIBarButtonItem) {
+        let currentVC = listVC.view.isDescendant(of: view) ? listVC : mapVC
+        let destinationVC: UIViewController
+            
+        if currentVC === listVC {
+            sender.title = "List"
+            destinationVC = mapVC
+        } else {
+            sender.title = "Map"
+            destinationVC = listVC
+        }
+        
+        transition(from: currentVC, to: destinationVC, duration: 1.0, options: .transitionFlipFromLeft, animations: {
+            //
+        }) { finished in
+            
+        }
+    }
+}
+
