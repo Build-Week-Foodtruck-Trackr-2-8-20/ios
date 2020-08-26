a9e080ec8e95f2ee111b47bb045c82e28c00bbc6
Shawn Gee <shawngee@gmail.com>
2020-08-25 23:35:58 -0400

    add rough setup to display pins for trucks

--- FoodTruckTracker/FoodTruckTracker/Search/Map/SearchResultsMapViewController.swift
+++ FoodTruckTracker/FoodTruckTracker/Search/Map/SearchResultsMapViewController.swift
@@ -6,25 +6,114 @@
 //  Copyright © 2020 Josh Kocsis. All rights reserved.
 //
 
+import CoreData
+import MapKit
 import UIKit
 
 class SearchResultsMapViewController: UIViewController {
-
+    @IBOutlet private var mapView: MKMapView!
+    
+    private lazy var fetchedResultsController: NSFetchedResultsController<Truck> = {
+        let request: NSFetchRequest<Truck> = Truck.fetchRequest()
+        request.sortDescriptors = [
+            .init(key: "truckName", ascending: true),
+        ]
+        let context = CoreDataStack.shared.mainContext
+        
+        let frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        frc.delegate = self
+        try? frc.performFetch()
+        
+        return frc
+    }()
+    
+    private var displayedAnnotations: [MKAnnotation]? {
+        willSet {
+            if let currentAnnotations = displayedAnnotations {
+                mapView.removeAnnotations(currentAnnotations)
+            }
+        }
+        didSet {
+            if let newAnnotations = displayedAnnotations {
+                mapView.addAnnotations(newAnnotations)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        displayedAnnotations = fetchedResultsController.fetchedObjects
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Adds 50 trucks to core data for testing
+    @IBAction func seedTrucks(_ sender: UIButton) {
+        for _ in 0..<50 {
+            let lat = Double.random(in: 32 ... 34)
+            let lon = Double.random(in: -82 ... -80)
+            
+            Truck(cuisineType: "American",
+                  truckName: "WHATABURGER",
+                  customerRating: 5,
+                  customerRatingAvg: 5,
+                  imageOfTruck: "https://www.kltv.com/resizer/or5mqVfPSUFp92P-ZnRxmQ5btds=/1200x600/cloudfront-us-east-1.images.arcpublishing.com/raycom/MR4RBHVK3JBSZOUZFXHVIFZAJY.jpeg",
+                  truckLatitude: lat,
+                  truckLongitude: lon)
+        }
+        
+        try? CoreDataStack.shared.save()
     }
-    */
+    
+    /// Removes all currently fetched trucks from core data
+    @IBAction func deleteTrucks(_ sender: UIButton) {
+        guard let trucks = fetchedResultsController.fetchedObjects else { return }
+        
+        for truck in trucks {
+            CoreDataStack.shared.mainContext.delete(truck)
+        }
+        try? CoreDataStack.shared.save()
+    }
+}
 
+// MARK: - Fetched Results Controller Delegate
+
+extension SearchResultsMapViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        
+        guard let truck = anObject as? Truck else { return }
+        
+        switch type {
+        case .insert:
+            mapView.addAnnotation(truck)
+        case .update:
+            mapView.removeAnnotation(truck)
+            mapView.addAnnotation(truck)
+        case .move:
+            break
+        case .delete:
+            mapView.removeAnnotation(truck)
+        @unknown default:
+            break
+        }
+    }
+}
+
+
+extension Truck: MKAnnotation {
+    /// Provides a `CLLocationCoordinate2D` based on a truck's latitude and longitude
+    public var coordinate: CLLocationCoordinate2D {
+        let lat = CLLocationDegrees(truckLatitude)
+        let lon = CLLocationDegrees(truckLongitude)
+        return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+    }
+    
+    /// Ensures that if a truck's longitude or latitude changes, the coordinate updates
+    class func keyPathsForValuesAffectingCoordinate() -> Set<String> {
+        Set<String>([#keyPath(truckLatitude), #keyPath(truckLongitude)])
+    }
 }
