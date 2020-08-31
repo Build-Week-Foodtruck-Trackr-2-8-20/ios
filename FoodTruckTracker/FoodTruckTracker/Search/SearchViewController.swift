a961503bed9d954e1c1f208b6a42f34711a7619c
Shawn Gee <shawngee@gmail.com>
2020-08-31 03:31:21 -0400

    move UIView extension to helpers

--- FoodTruckTracker/FoodTruckTracker/Search/SearchViewController.swift
+++ FoodTruckTracker/FoodTruckTracker/Search/SearchViewController.swift
@@ -132,35 +132,3 @@ protocol SearchResultsViewController: UIViewController, NSFetchedResultsControll
 }
 extension SearchResultsListViewController: SearchResultsViewController {}
 extension SearchResultsMapViewController: SearchResultsViewController {}
-
-
-extension UIView {
-    /// Fills the view's superview completely.
-    ///
-    /// Also sets `translatesAutoResizingMaskIntoConstraints` to false.
-    /// - Precondition: View must be embedded in a superview.
-    func fillSuperview(respectingSafeArea: Bool = false) {
-        guard let superview = superview else {
-            assertionFailure("\(Self.self) has no superview to fill")
-            return
-        }
-        
-        self.translatesAutoresizingMaskIntoConstraints = false
-        
-        if respectingSafeArea {
-            NSLayoutConstraint.activate([
-                self.topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor),
-                self.bottomAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.bottomAnchor),
-                self.leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor),
-                self.trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor),
-            ])
-        } else {
-            NSLayoutConstraint.activate([
-                self.topAnchor.constraint(equalTo: superview.topAnchor),
-                self.bottomAnchor.constraint(equalTo: superview.bottomAnchor),
-                self.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
-                self.trailingAnchor.constraint(equalTo: superview.trailingAnchor),
-            ])
-        }
-    }
-}
